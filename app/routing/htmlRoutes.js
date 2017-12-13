0ec5eae389764c0e3a41506fd8cb5990fcb9a310
Denny Wu <denny.wu87@gmail.com>
2017-12-13 14:41:37 -0800

    Wrote code for routes to home and survey html files

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,17 @@
+var path = require("path")
+
+module.exports = function(app) {
+
+	app.get("/survey.html", function(req, res) {
+		res.sendFile(path.join(__dirname, "/../public/survey.html"))
+	})
+
+	app.get("/home-styles.css", function(req, res) {
+		res.sendFile(path.join(__dirname, "/../public/home-styles.css"))
+	})
+
+	app.get("*", function(req, res) {
+		res.sendFile(path.join(__dirname, "/../public/home.html"))
+	})
+	
+}
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path")
 
 var app = express();
 
@@ -9,8 +8,8 @@ var PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+// require("./routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
