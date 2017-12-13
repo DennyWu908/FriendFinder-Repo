6400d64af6331d889a528fa4a96d5c3b17f698ce
Denny Wu <denny.wu87@gmail.com>
2017-12-13 15:10:14 -0800

    Added file for js code behind html pages

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -10,6 +10,10 @@ module.exports = function(app) {
 		res.sendFile(path.join(__dirname, "/../public/home-styles.css"))
 	})
 
+	app.get("/js/appScript.js", function(req, res) {
+		res.sendFile(path.join(__dirname, "/../public/js/appScript.js"))
+	})
+
 	app.get("*", function(req, res) {
 		res.sendFile(path.join(__dirname, "/../public/home.html"))
 	})
