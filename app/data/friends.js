07e7d3a61173c2547b74b112ddac7f31304cf0b1
Denny Wu <denny.wu87@gmail.com>
2017-12-28 01:21:15 -0800

    Adding more dummy data to friends js file

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,5 +3,15 @@ var friendsList = [
 		name: "Fred",
 		photo: ("photo link"),
 		scores: [1, 4, 3, 5, 1, 4, 3, 2, 5, 1]
+	},
+	{
+		name: "Oswald",
+		photo: ("photo link"),
+		scores: [2, 3, 4, 5, 1, 5, 4, 1, 3, 2]
+	},
+	{
+		name: "Shirley",
+		photo: ("photo link"),
+		scores: [5, 2, 5, 3, 4, 1, 1, 1, 2, 2]
 	}
 ];
